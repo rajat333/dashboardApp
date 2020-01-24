0583ab14ab7784dc7b287a10ee0e7a4065e9ed2b
Rajat Khurana <rajatkhuranak@gmail.com>
2020-01-24 22:46:26 +0530

    Added changes for dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import "./Dashboard.css";
-// import { Link } from 'react-router-dom';
-// import { history } from "./history/history";
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         console.log("this dashboard0");
         this.state = {
-            email: '',
-            password: ''
+            selectedUser :{ },
+            enablePopUp:false,
         };
     }
     
@@ -20,49 +18,48 @@ class Dashboard extends Component {
         <div className="FormCenter Dashboard">
         
         <div class="sidenav">
-  <a href="#about">About</a>
-  <a href="#services">Services</a>
-  <a href="#clients">Clients</a>
-  <a href="#contact">Contact</a>
-</div>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#clients">Clients</a>
+          <a href="#contact">Contact</a>
+        </div>
           <div className="main">
           <table className="table table-striped">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-        <td>
-          <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
-        </td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-        <td>
-          <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
-        </td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-        <td>
-          <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-       
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john@example.com</td>
+              <td>
+                <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
+              </td>
+            </tr>
+            <tr>
+              <td>Mary</td>
+              <td>Moe</td>
+              <td>mary@example.com</td>
+              <td>
+                <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
+              </td>
+            </tr>
+            <tr>
+              <td>July</td>
+              <td>Dooley</td>
+              <td>july@example.com</td>
+              <td>
+                <a className="add" title="Add" data-toggle="tooltip">  <i class="fas fa-file"></i></a>
+              </td>
+              </tr>
+            </tbody>
+          </table>
           </div>
         </div>
         );
